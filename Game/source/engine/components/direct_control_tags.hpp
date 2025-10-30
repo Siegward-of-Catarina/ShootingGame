c8770746290549b6eeac83c2533ed66c99de40e2
Siegward-of-Catarina <kurama354@icloud.com>
2025-10-31 03:04:59 +0900

    gitの設定変更

---
+++ Game/source/engine/components/direct_control_tags.hpp
@@ -0,0 +1,14 @@
+﻿#pragma once
+namespace sdl_engine
+{
+   // 　transformを直接操作するタグコンポーネント群
+   struct DirectMovementTag
+   {
+   };
+   struct DirectRotateTag
+   {
+   };
+   struct DirectScaleTag
+   {
+   };
+}    // namespace sdl_engine
---
+++ Game/source/engine/components/sprite_anim.hpp
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include <SDL3/SDL_rect.h>
+#include <engine/core/forward_declarations.hpp>
+#include <engine/utils/json_utilities.hpp>
+#include <vector>
+
+namespace sdl_engine
+{
+   // スプライトアニメーションの状態
+   struct SpriteAnim
+   {
+      entt::resource<SpriteAnimResource> sprite_anim;
+      u32                                current_frame {};
+      f64                                elapsed_time {};    // 経過秒
+      bool                               is_playing {};
+   };
+
+   // JSON からアニメ資源をロードして初期化
+   SpriteAnim createSpriteAnim( ResourceManager& resource_manager_, const json& data_ );
+
+   // 既存リソースから初期化
+   SpriteAnim
+   createSpriteAnim( entt::resource<SpriteAnimResource> resource_, u32 current_frame_ = 0, bool is_playing_ = true );
+}    // namespace sdl_engine
